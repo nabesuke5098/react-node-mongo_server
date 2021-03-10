7d4ea6f2af48fcb0a2e5bb3237ea139b5806365d
nabesuke <nabesuke5098@gmail.com>
2021-03-10 10:48:17 +0900

    [modified]記入間違いの修正
    ACCESS_TOKEN_SECRET

--- twitter/twitter.js
+++ twitter/twitter.js
@@ -1,20 +1,20 @@
-const Twitter = require('twitter');
+const twitter = require('twitter');
 const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '../.env') });
 
-const client = new Twitter({
+const client = new twitter({
   consumer_key: process.env.API_KEY ,
   consumer_secret: process.env.API_KEY_SECRET ,
   access_token_key: process.env.ACCESS_TOKEN ,
-  access_token_secret: process.env.API_KEY_SECRET ,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET ,
 });
 
-var params = {screen_name: 'kenken5018'};
+var params = {screen_name: '@kenken5018'};
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
     console.log(tweets);
   } else {
-    console.log('error');
+    console.log(error);
   }
 });
 // module.exports.client = client;
